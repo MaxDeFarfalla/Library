f2959496653946eb5d8d8b1b699d65e5a993b77d
MaxDeFarfalla <Maximilian.defarfalla@elev.ga.ntig.se>
2024-01-30 12:16:39 +0100

    Fixar bookReturn metoden lite strul med jämförelse mellan två datum dock framstår funktionen isAfter att fungera

--- src/BorrowableBook.java
+++ src/BorrowableBook.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.Scanner;
 
 public class BorrowableBook extends Book {
@@ -15,24 +16,19 @@ public class BorrowableBook extends Book {
     //Checks if book is borrowable and sets returnDate if borrowed
     private void borrowBook() {
         Scanner bookScanner = new Scanner(System.in);
-        boolean borrowable = false;
         String answer;
-
         generateData();
 
 
         if (dateOfReturn == null) {
-            borrowable = true;
 
-        }
-
-        if (borrowable == true) {
             System.out.println("Wish to borrow? " + name + " of " + author.getName());
             answer = bookScanner.nextLine().toLowerCase();
-            if (answer == "y" || answer == "yes") {
+
+            if (answer.equals("y") || answer.equals("yes")) {
                 dateOfReturn = LocalDate.now().plusDays(20);
 
-                System.out.println("Hope you enjoy - Book is to be returned within " + dateOfReturn);
+                System.out.println("Hope you enjoy - Book is to be returned at latest " + dateOfReturn);
             } else {
                 System.out.println("Have a good day");
             }
@@ -41,6 +37,19 @@ public class BorrowableBook extends Book {
             System.out.println("Not available - Have a good day");
         }
 
+    }
+
+    private void bookReturn(){
+        System.out.println("Wish to return "+ name +"?");
+        Scanner bookScanner = new Scanner(System.in);
+        String answer = bookScanner.nextLine().toLowerCase();
+
+        if (answer.equals("y") || (answer.equals("yes") && dateOfReturn.isAfter(LocalDate.now()))){
+
+        }
+        else (){
+
+        }
     }
 
     private void generateData() {
--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 public class Library {
 
     public static void Start(){
+        Scanner libScanner = new Scanner(System.in);
+        String libAnswer = libScanner.next().toLowerCase();
+  /*
+  //test code
      BorrowableBook b = new BorrowableBook();
      b.borrow();
+*/
 
+        System.out.println("1 - Browse collections");
+        System.out.println("2 - Borrow book");
+        System.out.println("3 - Return book");
+        System.out.println("4 - New book entry");   // Will be altered so that lib personal only
+
+        switch (libAnswer){
+            case "1":
+
+                break;
+
+            case "2":
+
+                break;
+
+            case "3":
+
+                break;
+
+            case "4":
+
+                break;
+        }
 
     }
 }
